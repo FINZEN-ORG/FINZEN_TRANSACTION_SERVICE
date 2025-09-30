afcf16dd8de402b7641aec372a74b77514de4f08
LaaSofiaa <laura.gil-c@mail.escuelaing.edu.co>
2025-09-30 16:06:52 -0500

    add: first config

---
+++ src/main/java/eci/ieti/FinzenTransactionService/FinzenTransactionService/FinzenTransactionServiceApplication.java
@@ -0,0 +1,13 @@
+package eci.ieti.FinzenTransactionService.FinzenTransactionService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinzenTransactionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinzenTransactionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eci/ieti/FinzenTransactionService/FinzenTransactionService/FinzenTransactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.ieti.FinzenTransactionService.FinzenTransactionService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinzenTransactionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
